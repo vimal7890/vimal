61b51151436029c77c659867390c7d9e230aab8e
Vimal <ucfavvi@ucl.ac.uk>
2026-04-10 15:18:51 +0100

    Add World Methodist Council denomination page

---
+++ religious-denominations/world-methodist-council-data.js
@@ -0,0 +1,141 @@
+window.wmcMemberRowsRaw = `
+International|Methodist|United Methodist Church (Central African Conference; Central and Southern European Central Conference; Congo Central Conference; German Central Conference; Northern Europe Central Conference; Philippines Central Conference; West Africa Central Conference; US Conferences; Liberia Conference)|7,607,194|2024
+International|Methodist|Church of the Nazarene|2,666,845|2021
+International|Methodist|African Methodist Episcopal Zion Church|1,432,795|2014
+International|Methodist|Wesleyan Church|140,954|2017
+International|Methodist|Free Methodist Church|1,547,820|2018
+International|Methodist|African Methodist Episcopal Church|2,500,000|2014
+South Africa, Lesotho, Eswatini, Mozambique and Namibia|Methodist|Methodist Church of Southern Africa|2,600,000|2014
+Argentina|Methodist|Evangelical Methodist Church in Argentina|5,599|2017
+Australia|Methodist|Chinese Methodist Church in Australia|3,588|2014
+Australia|Methodist|Wesleyan Methodist Church of Australia|2,452|2014
+Australia|United Churches (Presbyterians, Methodists and Congregationalists)|Uniting Church in Australia|243,000|2018
+Bahamas|Methodist|Bahamas Conference of the Methodist Church|1,900|2014
+Bangladesh|Methodist|Bangladesh Methodist Church|18,201|2014
+Belgium|United Churches (Continental Reformed and Methodist)|United Protestant Church in Belgium|3,401|2014
+Benin|Methodist|Protestant Methodist Church in Benin|90,000|2014
+Bolivia|Methodist|Evangelical Methodist Church in Bolivia|9,053|2014
+Brazil|Methodist|Methodist Church in Brazil|262,449|2022
+Canada|United Churches (Presbyterians, Congregationalists and Methodists)|United Church of Canada|325,315|2023
+Caribbean|Methodist|Methodist Church in the Caribbean and the Americas|62,120|2014
+Chile|Methodist|Methodist Church in Chile|9,882|2014
+China (People's Republic of China)|United Churches (Presbyterians, Congregationalists, Baptists and Methodists)|Hong Kong Council of the Church of Christ in China|36,000|2016
+China (People's Republic of China)|Methodist|Methodist Church, Hong Kong|12,000|2014
+Colombia|Methodist|Colombian Methodist Church|1,000|2014
+South Korea|Methodist|Korean Methodist Church|1,106,385|2023
+Costa Rica|Methodist|Evangelical Methodist Church in Costa Rica|16,000|2014
+Cuba|Methodist|Methodist Church in Cuba|33,000|2014
+Congo (Democratic Republic of Congo)|Methodist|Free Methodist Church of the Democratic Republic of the Congo|151,695|2014
+Dominican Republic|Methodist|Evangelical Church of the Dominican Republic|10,000|2014
+Ecuador|United Churches (Presbyterians and Methodists)|United Evangelical Church of Ecuador|1,500|2014
+Spain|United Churches (Presbyterians, Congregationalists, Waldensians, Methodists and Lutherans)|Spanish Evangelical Church|3,000|2014
+Fiji|Methodist|Methodist Church of Fiji and Rotuma|212,860|2014
+Gambia|Methodist|Gambia Methodist Church|2,000|2014
+Ghana|Methodist|Methodist Church Ghana|634,689|2014
+India|Methodist|Methodist Church in India|648,000|2014
+India|United Churches (Presbyterians, Anglicans, Methodists and Disciples of Christ)|Church of North India|2,300,000|2025
+India|United Churches (Presbyterians, Congregationalists, Continental Reformed, Anglicans and Methodists)|Church of South India|5,000,000|2020
+Indonesia|Methodist|Methodist Church in Indonesia|119,000|2014
+Ireland|Methodist|Methodist Church in Ireland|15,000|2014
+Italy|Methodist|Methodist Evangelical Church in Italy|4,000|2014
+Kenya|Methodist|Methodist Church in Kenya|450,000|2014
+Malaysia|Methodist|Methodist Church in Malaysia|97,197|2014
+Mexico|Methodist|Methodist Church of Mexico|40,000|2014
+Myanmar|Methodist|Methodist Church, Lower Myanmar|2,300|2014
+Myanmar|Methodist|Methodist Church, Upper Myanmar|27,543|2014
+Nepal|Methodist|Nepal Methodist Church|264|2014
+Nigeria|Methodist|Methodist Church of Nigeria|2,000,000|2014
+New Zealand|Methodist|Methodist Church of New Zealand|14,736|2014
+New Zealand|Methodist|New Zealand Wesleyan Methodist Church|794|2014
+Pakistan|United Churches (Presbyterians, Methodists, Lutherans and Anglicans)|Church of Pakistan|1,900,000|2020
+Panama|Methodist|Evangelical Methodist Church of Panama|1,300|2014
+Paraguay|Methodist|Methodist Evangelical Community of Paraguay|1,025|2014
+Peru|Methodist|Methodist Church of Peru|32,000|2014
+Philippines|Methodist|Evangelical Methodist Church in the Philippine Islands|27,793|2020
+Philippines|United Churches (Presbyterians, Baptists, Disciples of Christ and Methodists)|United Church of Christ in the Philippines|470,792|2020
+Portugal|Methodist|Portuguese Evangelical Methodist Church|1,200|2014
+Puerto Rico|Methodist|Puerto Rico Methodist Church|12,000|2014
+Rwanda|Methodist|Rwanda Free Methodist Church|108,559|2014
+Samoa|Methodist|Samoa Methodist Church|35,983|2014
+Sierra Leone|Methodist|Sierra Leone Methodist Church|50,000|2014
+Sierra Leone|Methodist|West African Methodist Church in Sierra Leone|4,000|2014
+Singapore|Methodist|Methodist Church in Singapore|38,000|2014
+Sri Lanka|Methodist|Methodist Church in Sri Lanka|25,000|2014
+Sweden|United Churches (Baptists, PT and Methodist)|Uniting Church in Sweden|58,569|2021
+Taiwan (Republic of China)|Methodist|Methodist Church in the Republic of China|4,500|2014
+Tanzania|Methodist|Tanzania Methodist Church|3,681|2014
+Thailand|Methodist|Thailand Union Methodist Church|3,000|2024
+Togo|Methodist|Togo Methodist Church|75,000|2014
+Tonga|Methodist|Free Wesleyan Church|38,692|2014
+United Kingdom|Methodist|Methodist Church in Great Britain|136,891|2022
+United States of America|Methodist|Christian Methodist Episcopal Church|858,670|2014
+Zambia|United Churches (Presbyterians and Methodists)|United Church of Zambia|4,000,000|2024
+Zimbabwe|Methodist|Zimbabwe Methodist Church|111,723|2014
+Zimbabwe|Methodist|African Methodist Church of Zimbabwe|12,000|2014
+Global|Sub-total|Methodists|25,945,040|2014-2025
+Global|Sub-total|United Churches|14,341,577|2014-2025
+Global|Total|World Methodist Council|40,286,617|2014-2025
+`.trim();
+
+window.wmcMapTerritoriesRaw = `
+South Africa
+Lesotho
+Eswatini
+Mozambique
+Namibia
+Argentina
+Australia
+Bahamas
+Bangladesh
+Belgium
+Benin
+Bolivia
+Brazil
+Canada
+Chile
+China (People's Republic of China)
+Colombia
+South Korea
+Costa Rica
+Cuba
+Congo (Democratic Republic of Congo)
+Dominican Republic
+Ecuador
+Spain
+Fiji
+Gambia
+Ghana
+India
+Indonesia
+Ireland
+Italy
+Kenya
+Malaysia
+Mexico
+Myanmar
+Nepal
+Nigeria
+New Zealand
+Pakistan
+Panama
+Paraguay
+Peru
+Philippines
+Portugal
+Puerto Rico
+Rwanda
+Samoa
+Sierra Leone
+Singapore
+Sri Lanka
+Sweden
+Taiwan (Republic of China)
+Tanzania
+Thailand
+Togo
+Tonga
+United Kingdom
+United States of America
+Zambia
+Zimbabwe
+`.trim();
