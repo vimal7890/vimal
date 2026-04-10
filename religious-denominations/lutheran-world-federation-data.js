fc77eeab478b5960500b0199b955d3df3fa93aae
Vimal <ucfavvi@ucl.ac.uk>
2026-04-10 15:53:06 +0100

    add Lutheran World Federation members page

---
+++ religious-denominations/lutheran-world-federation-data.js
@@ -0,0 +1,260 @@
+window.lwfMemberRowsRaw = `
+Angola|Evangelical Lutheran Church of Angola
+Argentina|Evangelical Church of the River Plate
+Argentina|United Evangelical Lutheran Church
+Australia|Lutheran Church of Australia
+Austria|Protestant Church of the Augsburg Confession in Austria
+Bangladesh|Bangladesh Lutheran Church
+Bangladesh|Bangladesh Northern Evangelical Lutheran Church
+Belarus|Evangelical Lutheran Church in Russia and Other States
+Belgium|Lutheran Church of Belgium: Arlon and Christian Mission
+Bolivia|Bolivian Evangelical Lutheran Church
+Bolivia|German-Speaking Evangelical Lutheran Congregation in Bolivia
+Botswana|Evangelical Lutheran Church in Botswana
+Brazil|Evangelical Church of the Lutheran Confession in Brazil
+Cameroon|Church of the Lutheran Brethren of Cameroon
+Cameroon|Evangelical Lutheran Church of Cameroon
+Canada|Evangelical Lutheran Church in Canada
+Central African Republic|Lutheran Church of the Central African Republic
+Chile|Evangelical Lutheran Church in Chile
+Chile|Lutheran Church in Chile
+China (Hong Kong SAR)|Chinese Rhenish Church Hong Kong Synod
+China (Hong Kong SAR)|Evangelical Lutheran Church of Hong Kong
+China (Hong Kong SAR)|Hong Kong and Macau Lutheran Church
+China (Hong Kong SAR)|Tsung Tsin Mission of Hong Kong
+Colombia|Evangelical Lutheran Church of Colombia
+Colombia|St. Martin's Lutheran Congregation
+Colombia|St. Matthew's Lutheran Congregation
+Congo, Democratic Republic of|Evangelical Lutheran Church in Congo
+Costa Rica|Evangelical Lutheran Church of Costa Rica
+Costa Rica|Lutheran Costa Rican Church
+Croatia|Evangelical Church in the Republic of Croatia
+Czechia|Evangelical Church of Czech Brethren
+Czechia|Silesian Evangelical Church of the Augsburg Confession
+Denmark|Evangelical Lutheran Church in Denmark
+Ecuador|Evangelical Lutheran Church in Ecuador
+El Salvador|Salvadoran Lutheran Church
+Eritrea|Evangelical Church of Eritrea
+Estonia|Estonian Evangelical Lutheran Church
+Ethiopia|Ethiopian Evangelical Church Mekane Yesus
+Finland|Evangelical Lutheran Church of Finland
+France|Malagasy Protestant Church in France
+France|Union of Protestant Churches of Alsace and Lorraine
+France|United Protestant Church of France
+Georgia|Evangelical Lutheran Church in Russia and Other States
+Germany|Church of Lippe
+Germany|Evangelical Church in Central Germany
+Germany|Evangelical Lutheran Church in Baden
+Germany|Evangelical Lutheran Church in Bavaria
+Germany|Evangelical Lutheran Church in Brunswick
+Germany|Evangelical Lutheran Church in Northern Germany
+Germany|Evangelical Lutheran Church in Oldenburg
+Germany|Evangelical Lutheran Church of Hanover
+Germany|Evangelical Lutheran Church of Saxony
+Germany|Evangelical Lutheran Church of Schaumburg-Lippe
+Germany|Evangelical-Lutheran Church in Wurttemberg
+Germany|Latvian Evangelical Lutheran Church Abroad
+Ghana|Evangelical Lutheran Church of Ghana
+Guatemala|Evangelical Lutheran Congregation "La Epifania"
+Guyana|Evangelical Lutheran Church in Guyana
+Honduras|Christian Lutheran Church of Honduras
+Hungary|Evangelical Lutheran Church in Hungary
+Iceland|Evangelical Lutheran Church of Iceland
+India|Andhra Evangelical Lutheran Church
+India|Arcot Lutheran Church
+India|Evangelical Lutheran Church in Madhya Pradesh
+India|Evangelical Lutheran Church in the Himalayan States
+India|Good Shepherd Evangelical Lutheran Church
+India|Gossner Evangelical Lutheran Church in Chotanagpur and Assam
+India|India Evangelical Lutheran Church
+India|Jeypore Evangelical Lutheran Church
+India|Northern Evangelical Lutheran Church
+India|South Andhra Lutheran Church
+India|Tamil Evangelical Lutheran Church
+Indonesia|Batak Christian Community Church
+Indonesia|Christian Communion of Indonesia Church in Nias
+Indonesia|Christian Protestant Angkola Church
+Indonesia|Christian Protestant Church in Indonesia
+Indonesia|Indonesian Christian Church
+Indonesia|Indonesian Christian Luther Church
+Indonesia|Pakpak Dairi Christian Protestant Church
+Indonesia|Protestant Christian Batak Church
+Indonesia|Protestant Christian Church
+Indonesia|Protestant Christian Church in Mentawai
+Indonesia|Simalungun Protestant Christian Church
+Indonesia|United Protestant Church
+Ireland|Lutheran Church in Ireland
+Italy|Lutheran Evangelical Church in Italy
+Japan|Japan Evangelical Lutheran Church
+Japan|Japan Lutheran Church
+Japan|Kinki Evangelical Lutheran Church
+Jordan|Evangelical Lutheran Church in Jordan & the Holy Land
+Kazakhstan|Evangelical Lutheran Church in Russia and Other States
+Kenya|Evangelical Lutheran Church in Kenya
+Kenya|Kenya Evangelical Lutheran Church
+Korea, Republic|Lutheran Church in Korea
+Kyrgyzstan|Evangelical Lutheran Church in Russia and Other States
+Latvia|Evangelical Lutheran Church of Latvia
+Liberia|Lutheran Church in Liberia
+Liechtenstein|Federation of Evangelical Lutheran Churches in Switzerland and the Principality of Liechtenstein
+Lithuania|Evangelical Lutheran Church of Lithuania
+Madagascar|Malagasy Lutheran Church
+Malawi|Evangelical Lutheran Church in Malawi
+Malaysia|Basel Christian Church of Malaysia
+Malaysia|Evangelical Lutheran Church in Malaysia
+Malaysia|Lutheran Church in Malaysia and Singapore
+Malaysia|Protestant Church in Sabah
+Mexico|German-Speaking Evangelical Congregation in Mexico
+Mexico|Mexican Lutheran Church
+Mozambique|Evangelical Lutheran Church in Mozambique
+Myanmar|Evangelical Lutheran Church in Myanmar
+Myanmar|Lutheran Church of Myanmar
+Myanmar|Mara Evangelical Church
+Myanmar|Myanmar Lutheran Church
+Namibia|Evangelical Lutheran Church in Namibia
+Namibia|Evangelical Lutheran Church in Namibia (ELCIN - GELC)
+Namibia|Evangelical Lutheran Church in the Republic of Namibia (ELCRN)
+Nepal|Nepal Northern Evangelical Lutheran Church
+Netherlands|Protestant Church in the Netherlands
+Nicaragua|Nicaraguan Lutheran Church of Faith and Hope
+Nigeria|Lutheran Church in Nigeria
+Nigeria|Lutheran Church of Christ in Nigeria
+Norway|Church of Norway
+Norway|Evangelical Lutheran Free Church of Norway
+Palestine|Evangelical Lutheran Church in Jordan & the Holy Land
+Papua New Guinea|Evangelical Lutheran Church of Papua New Guinea
+Papua New Guinea|Gutnius Lutheran Church
+Peru|Evangelical Lutheran Church in Peru
+Peru|Peruvian Lutheran Evangelical Church
+Philippines|Lutheran Church in the Philippines
+Poland|Evangelical Church of Augsburg Confession in Poland
+Romania|Evangelical Church of the Augsburg Confession in Romania
+Romania|Evangelical Lutheran Church in Romania
+Russia|Evangelical Lutheran Church in Russia and Other States
+Russia|Evangelical Lutheran Church of Ingria in Russia
+Rwanda|Lutheran Church of Rwanda
+Senegal|Lutheran Church of Senegal
+Serbia|Slovak Evangelical Church of the Augsburg Confession in Serbia
+Sierra Leone|Evangelical Lutheran Church in Sierra Leone
+Singapore|Lutheran Church in Singapore
+Slovak Republic|Evangelical Church of the Augsburg Confession in Slovak Republic
+Slovenia|Evangelical Church of the Augsburg Confession in Slovenia
+South Africa|Evangelical Lutheran Church in Southern Africa
+South Africa|Evangelical Lutheran Church in Southern Africa (Cape Church)
+South Africa|Evangelical Lutheran Church in Southern Africa (N-T)
+South Africa|Moravian Church in South Africa
+Sri Lanka|Lanka Lutheran Church
+Suriname|Evangelical Lutheran Church in Suriname
+Sweden|Church of Sweden
+Switzerland|Federation of Evangelical Lutheran Churches in Switzerland and the Principality of Liechtenstein
+Taiwan (Republic of China)|Lutheran Church of Taiwan
+Taiwan (Republic of China)|Taiwan Lutheran Church
+Tanzania|Evangelical Lutheran Church in Tanzania
+Thailand|Evangelical Lutheran Church in Thailand
+Ukraine|Evangelical Lutheran Church in Russia and Other States
+United Kingdom|Lutheran Church in Great Britain
+United Kingdom|Lutheran Council of Great Britain
+United States of America|Evangelical Lutheran Church in America
+Uruguay|Evangelical Church of the River Plate
+Uruguay|Evangelical Lutheran Church in Uruguay
+Uzbekistan|Evangelical Lutheran Church in Russia and Other States
+Venezuela|Evangelical Lutheran Church in Venezuela
+Venezuela|Lutheran Church of Venezuela
+Zambia|Evangelical Lutheran Church in Zambia
+Zimbabwe|Evangelical Lutheran Church in Zimbabwe
+`.trim();
+
+window.lwfMapTerritoriesRaw = `
+Angola
+Argentina
+Australia
+Austria
+Bangladesh
+Belarus
+Belgium
+Bolivia
+Botswana
+Brazil
+Cameroon
+Canada
+Central African Republic
+Chile
+China (Hong Kong SAR)
+Colombia
+Congo, Democratic Republic of
+Costa Rica
+Croatia
+Czechia
+Denmark
+Ecuador
+El Salvador
+Eritrea
+Estonia
+Ethiopia
+Finland
+France
+Georgia
+Germany
+Ghana
+Guatemala
+Guyana
+Honduras
+Hungary
+Iceland
+India
+Indonesia
+Ireland
+Italy
+Japan
+Jordan
+Kazakhstan
+Kenya
+Korea, Republic
+Kyrgyzstan
+Latvia
+Liberia
+Liechtenstein
+Lithuania
+Madagascar
+Malawi
+Malaysia
+Mexico
+Mozambique
+Myanmar
+Namibia
+Nepal
+Netherlands
+Nicaragua
+Nigeria
+Norway
+Palestine
+Papua New Guinea
+Peru
+Philippines
+Poland
+Romania
+Russia
+Rwanda
+Senegal
+Serbia
+Sierra Leone
+Singapore
+Slovak Republic
+Slovenia
+South Africa
+Sri Lanka
+Suriname
+Sweden
+Switzerland
+Taiwan (Republic of China)
+Tanzania
+Thailand
+Ukraine
+United Kingdom
+United States of America
+Uruguay
+Uzbekistan
+Venezuela
+Zambia
+Zimbabwe
+`.trim();
