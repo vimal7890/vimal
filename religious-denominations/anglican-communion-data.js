77960bc6be3bb4f11967773d572110f55a28ebe2
Vimal <ucfavvi@ucl.ac.uk>
2026-04-10 13:09:11 +0100

    add Anglican Communion primate columns and tenure data

--- religious-denominations/anglican-communion-data.js
+++ religious-denominations/anglican-communion-data.js
@@ -48,6 +48,197 @@ Extraprovincial church|Lusitanian Catholic Apostolic Evangelical Church|Portugal
 Extraprovincial church|Spanish Reformed Episcopal Church|Spain
 `.trim();
 
+window.acLeadershipByChurch = {
+    "Episcopal/Anglican Province of Alexandria": {
+        leader: "Samy Fawzy / Archbishop of Alexandria",
+        since: "June 8th 2021"
+    },
+    "Anglican Church in Aotearoa, New Zealand and Polynesia": {
+        leader: "Don Tamihere; Sione Ulu'ilakepa; Justin Duckworth (co-primates)",
+        since: "n/a"
+    },
+    "Anglican Church of Australia": {
+        leader: "Mark Short / Primate of Australia",
+        since: "November 1st 2025"
+    },
+    "Church of Bangladesh": {
+        leader: "Samuel Sunil Mankhin / Moderator",
+        since: "December 5th 2018"
+    },
+    "Anglican Episcopal Church of Brazil": {
+        leader: "Marinez Rosa dos Santos Bassotto / Primate",
+        since: "November 23rd 2022"
+    },
+    "Province of the Anglican Church of Burundi": {
+        leader: "Sixbert Macumi / Archbishop of Burundi",
+        since: "August 21st 2021"
+    },
+    "Anglican Church of Canada": {
+        leader: "Shane Parker / Primate",
+        since: "June 29th 2025"
+    },
+    "Church of the Province of Central Africa": {
+        leader: "Albert Chama / Archbishop of Central Africa",
+        since: "March 20th 2011"
+    },
+    "Anglican Church in Central America": {
+        leader: "Juan David Alvarado / Primate of Central America",
+        since: "August 24th 2022"
+    },
+    "Anglican Church of Chile": {
+        leader: "Enrique Lago / Archbishop",
+        since: "January 12th 2025"
+    },
+    "Province of the Anglican Church of the Congo": {
+        leader: "Georges Titre Ande / Archbishop of the Congo",
+        since: "January 23rd 2022"
+    },
+    "Church of England": {
+        leader: "Sarah Mullally / Archbishop of Canterbury",
+        since: "January 28th 2026"
+    },
+    "Hong Kong Sheng Kung Hui": {
+        leader: "Andrew Chan / Archbishop of Hong Kong",
+        since: "January 3rd 2021"
+    },
+    "Church of the Province of the Indian Ocean": {
+        leader: "Gilbert Rateloson Rakotondravelo / Archbishop of the Indian Ocean",
+        since: "December 14th 2024"
+    },
+    "Church of Ireland": {
+        leader: "John McDowell / Archbishop of Armagh",
+        since: "April 28th 2020"
+    },
+    "Nippon Sei Ko Kai": {
+        leader: "David Eisho Uehara / Primate of Japan",
+        since: "May 30th 2024"
+    },
+    "Episcopal Church in Jerusalem and the Middle East": {
+        leader: "Hosam Naoum / President Bishop and Primate",
+        since: "May 12th 2023"
+    },
+    "Anglican Church of Kenya": {
+        leader: "Jackson Ole Sapit / Primate and Archbishop",
+        since: "July 3rd 2016"
+    },
+    "Anglican Church of Korea": {
+        leader: "Onesimus Park / Presiding Bishop",
+        since: "July 12th 2024"
+    },
+    "Anglican Church of Melanesia": {
+        leader: "Leonard Dawea / Archbishop of Melanesia",
+        since: "September 15th 2019"
+    },
+    "Anglican Church of Mexico": {
+        leader: "Enrique Trevino Cruz / Archbishop and Primate",
+        since: "June 11th 2022"
+    },
+    "Anglican Church of Mozambique and Angola": {
+        leader: "Vicente Msosa / Presiding Bishop",
+        since: "January 26th 2025"
+    },
+    "Church of the Province of Myanmar": {
+        leader: "Stephen Than Myint Oo / Archbishop of Myanmar",
+        since: "February 17th 2008"
+    },
+    "Church of Nigeria": {
+        leader: "Henry Ndukuba / Primate of All Nigeria",
+        since: "March 25th 2020"
+    },
+    "Church of North India": {
+        leader: "Paritosh Canning / Moderator",
+        since: "October 22nd 2025"
+    },
+    "Church of Pakistan": {
+        leader: "Azad Marshall / Moderator",
+        since: "May 14th 2021"
+    },
+    "Anglican Church of Papua New Guinea": {
+        leader: "Nathan Ingen / Senior Bishop (acting primate)",
+        since: "May 11th 2020"
+    },
+    "Episcopal Church in the Philippines": {
+        leader: "Nestor Poltic / Prime Bishop",
+        since: "March 12th 2025"
+    },
+    "Anglican Church of Rwanda": {
+        leader: "Laurent Mbanda / Archbishop of Rwanda",
+        since: "June 10th 2018"
+    },
+    "Scottish Episcopal Church": {
+        leader: "Mark Strange / Primus",
+        since: "June 27th 2017"
+    },
+    "Anglican Church of South America": {
+        leader: "Brian Williams / Presiding Bishop",
+        since: "May 7th 2023"
+    },
+    "Church of the Province of South East Asia": {
+        leader: "Titus Chung / Archbishop of South East Asia",
+        since: "January 23rd 2024"
+    },
+    "Church of South India": {
+        leader: "K. Reuben Mark / Moderator",
+        since: "July 21st 2025"
+    },
+    "Province of the Episcopal Church of South Sudan": {
+        leader: "Justin Badi Arama / Primate of South Sudan",
+        since: "April 22nd 2018"
+    },
+    "Anglican Church of Southern Africa": {
+        leader: "Thabo Makgoba / Archbishop of Cape Town",
+        since: "December 31st 2007"
+    },
+    "Province of the Episcopal Church of Sudan": {
+        leader: "Ezekiel Kondo / Primate of Sudan",
+        since: "July 30th 2017"
+    },
+    "Anglican Church of Tanzania": {
+        leader: "Maimbo Mndolwa / Archbishop of Tanzania",
+        since: "May 20th 2018"
+    },
+    "Church of Uganda": {
+        leader: "Stephen Kaziimba / Archbishop of Uganda",
+        since: "March 1st 2020"
+    },
+    "Episcopal Church": {
+        leader: "Sean W. Rowe / Presiding Bishop",
+        since: "November 1st 2024"
+    },
+    "Church in Wales": {
+        leader: "Cherry Vann / Archbishop of Wales",
+        since: "July 30th 2025"
+    },
+    "Church of the Province of West Africa": {
+        leader: "Cyril Kobina Ben-Smith / Primate of West Africa",
+        since: "June 3rd 2022"
+    },
+    "Church in the Province of the West Indies": {
+        leader: "Philip S. Wright / Archbishop of the West Indies",
+        since: "November 10th 2025"
+    },
+    "Anglican Church of Bermuda": {
+        leader: "Sarah Mullally / Archbishop of Canterbury (primatial authority)",
+        since: "January 28th 2026"
+    },
+    "Church of Ceylon": {
+        leader: "Sarah Mullally / Archbishop of Canterbury (primatial authority)",
+        since: "January 28th 2026"
+    },
+    "Parish of the Falkland Islands": {
+        leader: "Sarah Mullally / Archbishop of Canterbury (primatial authority)",
+        since: "January 28th 2026"
+    },
+    "Lusitanian Catholic Apostolic Evangelical Church": {
+        leader: "Sarah Mullally / Archbishop of Canterbury (primatial authority)",
+        since: "January 28th 2026"
+    },
+    "Spanish Reformed Episcopal Church": {
+        leader: "Sarah Mullally / Archbishop of Canterbury (primatial authority)",
+        since: "January 28th 2026"
+    }
+};
+
 window.acMapTerritoriesRaw = `
 Algeria
 Djibouti
