bf0f0670bbe3327117b12f0f15d138e9abbacc05
Vimal <ucfavvi@ucl.ac.uk>
2026-04-10 12:45:08 +0100

    add Anglican Communion page, members table, and map

---
+++ religious-denominations/anglican-communion-data.js
@@ -0,0 +1,196 @@
+window.acMemberRowsRaw = `
+Province|Episcopal/Anglican Province of Alexandria|Algeria, Djibouti, Egypt, Ethiopia, Eritrea, Libya, Somalia, Tunisia
+Province|Anglican Church in Aotearoa, New Zealand and Polynesia|New Zealand, Cook Islands, Fiji, Samoa, Tonga
+Province|Anglican Church of Australia|Australia
+Province|Church of Bangladesh|Bangladesh
+Province|Anglican Episcopal Church of Brazil|Brazil
+Province|Province of the Anglican Church of Burundi|Burundi
+Province|Anglican Church of Canada|Canada
+Province|Church of the Province of Central Africa|Botswana, Malawi, Zambia, Zimbabwe
+Province|Anglican Church in Central America|Costa Rica, El Salvador, Guatemala, Nicaragua, Panama
+Province|Anglican Church of Chile|Chile
+Province|Province of the Anglican Church of the Congo|Democratic Republic of the Congo, Republic of Congo
+Province|Church of England|England, Crown Dependencies, Europe
+Province|Hong Kong Sheng Kung Hui|Hong Kong, Macau
+Province|Church of the Province of the Indian Ocean|Madagascar, Mauritius, Seychelles
+Province|Church of Ireland|Republic of Ireland, Northern Ireland
+Province|Nippon Sei Ko Kai|Japan
+Province|Episcopal Church in Jerusalem and the Middle East|Bahrain, Cyprus, Iran, Iraq, Israel, Jordan, Kuwait, Lebanon, Oman, Palestine, Qatar, Saudi Arabia, Syria, United Arab Emirates, Yemen
+Province|Anglican Church of Kenya|Kenya
+Province|Anglican Church of Korea|South Korea, North Korea
+Province|Anglican Church of Melanesia|New Caledonia, Solomon Islands, Vanuatu
+Province|Anglican Church of Mexico|Mexico
+Province|Anglican Church of Mozambique and Angola|Angola and Mozambique
+Province|Church of the Province of Myanmar|Myanmar
+Province|Church of Nigeria|Nigeria
+Province|Church of North India|Bhutan, India
+Province|Church of Pakistan|Pakistan
+Province|Anglican Church of Papua New Guinea|Papua New Guinea
+Province|Episcopal Church in the Philippines|Philippines
+Province|Anglican Church of Rwanda|Rwanda
+Province|Scottish Episcopal Church|Scotland
+Province|Anglican Church of South America|Argentina, Bolivia, Paraguay, Peru, Uruguay
+Province|Church of the Province of South East Asia|Brunei, Cambodia, Indonesia, Laos, Malaysia, Nepal, Singapore, Thailand, Vietnam
+Province|Church of South India|India, Sri Lanka
+Province|Province of the Episcopal Church of South Sudan|South Sudan
+Province|Anglican Church of Southern Africa|Eswatini, Lesotho, Namibia, Saint Helena, South Africa
+Province|Province of the Episcopal Church of Sudan|Sudan
+Province|Anglican Church of Tanzania|Tanzania
+Province|Church of Uganda|Uganda
+Province|Episcopal Church|British Virgin Islands, Colombia, Cuba, Dominican Republic, Ecuador, Europe, Guam, Haiti, Honduras, Northern Mariana Islands, Puerto Rico, Taiwan, United States, United States Virgin Islands, Venezuela
+Province|Church in Wales|Wales
+Province|Church of the Province of West Africa|Cameroon, Cape Verde, Gambia, Ghana, Guinea, Liberia, Senegal, Sierra Leone
+Province|Church in the Province of the West Indies|Anguilla, Antigua and Barbuda, Aruba, Bahamas, Barbados, Belize, Cayman Islands, Dominica, Grenada, Guyana, Jamaica, Montserrat, Saba, Saint Barthelemy, Saint Kitts and Nevis, Saint Lucia, Saint Martin, Saint Vincent and the Grenadines, Sint Eustatius, Trinidad and Tobago, Turks and Caicos Islands
+Extraprovincial church|Anglican Church of Bermuda|Bermuda
+Extraprovincial church|Church of Ceylon|Sri Lanka
+Extraprovincial church|Parish of the Falkland Islands|Falkland Islands
+Extraprovincial church|Lusitanian Catholic Apostolic Evangelical Church|Portugal
+Extraprovincial church|Spanish Reformed Episcopal Church|Spain
+`.trim();
+
+window.acMapTerritoriesRaw = `
+Algeria
+Djibouti
+Egypt
+Ethiopia
+Eritrea
+Libya
+Somalia
+Tunisia
+New Zealand
+Cook Islands
+Fiji
+Samoa
+Tonga
+Australia
+Bangladesh
+Brazil
+Burundi
+Canada
+Botswana
+Malawi
+Zambia
+Zimbabwe
+Costa Rica
+El Salvador
+Guatemala
+Nicaragua
+Panama
+Chile
+Democratic Republic of the Congo
+Republic of Congo
+England
+Hong Kong
+Macau
+Madagascar
+Mauritius
+Seychelles
+Republic of Ireland
+Northern Ireland
+Japan
+Bahrain
+Cyprus
+Iran
+Iraq
+Israel
+Jordan
+Kuwait
+Lebanon
+Oman
+Palestine
+Qatar
+Saudi Arabia
+Syria
+United Arab Emirates
+Yemen
+Kenya
+South Korea
+North Korea
+New Caledonia
+Solomon Islands
+Vanuatu
+Mexico
+Angola
+Mozambique
+Myanmar
+Nigeria
+Bhutan
+India
+Pakistan
+Papua New Guinea
+Philippines
+Rwanda
+Scotland
+Argentina
+Bolivia
+Paraguay
+Peru
+Uruguay
+Brunei
+Cambodia
+Indonesia
+Laos
+Malaysia
+Nepal
+Singapore
+Thailand
+Vietnam
+Sri Lanka
+South Sudan
+Eswatini
+Lesotho
+Namibia
+Saint Helena
+South Africa
+Sudan
+Tanzania
+Uganda
+British Virgin Islands
+Colombia
+Cuba
+Dominican Republic
+Ecuador
+Guam
+Haiti
+Honduras
+Northern Mariana Islands
+Puerto Rico
+Taiwan
+United States
+United States Virgin Islands
+Venezuela
+Wales
+Cameroon
+Cape Verde
+Gambia
+Ghana
+Guinea
+Liberia
+Senegal
+Sierra Leone
+Anguilla
+Antigua and Barbuda
+Aruba
+Bahamas
+Barbados
+Belize
+Cayman Islands
+Dominica
+Grenada
+Guyana
+Jamaica
+Montserrat
+Saba
+Saint Barthelemy
+Saint Kitts and Nevis
+Saint Lucia
+Saint Martin
+Saint Vincent and the Grenadines
+Sint Eustatius
+Trinidad and Tobago
+Turks and Caicos Islands
+Bermuda
+Falkland Islands
+Portugal
+Spain
+`.trim();
